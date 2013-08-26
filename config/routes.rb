c4cac81af4220a288cc6614c45635282376585b9
shuoz <shuo.zhang.us@gmail.com>
2013-08-25 21:59:51 -0400

    second project version

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 AAAWebsite::Application.routes.draw do
+  resources :comments
+
+  devise_for :users
+
+  resources :events
+
  #get "aaa_core/index"
 
-  match '/events', :to => 'aaa_core#events'
+#  match '/events', :to => 'aaa_core#events'
   match '/shop', :to => 'aaa_core#shop'
   match '/discussion', :to => 'aaa_core#discussion'
 
